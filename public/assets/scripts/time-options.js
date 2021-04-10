7a5a2eeb8ac2b5e7dcf5d450cf65953bf249d26a
Fabinho Flauzino <fabioflauzino@hotmail.com>
2021-04-09 23:19:12 -0300

    Vinculando Firestore no projeto e trazendo dados do firebase na página de schedule-options

--- public/assets/scripts/time-options.js
+++ public/assets/scripts/time-options.js
@@ -1,89 +1,70 @@
 import { format, parse } from "date-fns"
-import { appendTemplate, getQueryString } from "./utils"
-import {ptBR} from 'date-fns/locale'
+import { ptBR } from 'date-fns/locale'
+import firebase from './firebase-app'
+import { appendTemplate, getFormValues, getQueryString, onSnapshotError, setFormValues } from "./utils"
 
-const data = [{
-    id:1,
-    value: '9:00'
-   }, {
-       id:2,
-       value:'10:00'
-   }, {
-       id:3,
-       value:'11:00'
-   }, {
-       id:4,
-       value:'12:00'
-   }, {
-       id:5,
-       value:'13:00'
-   }, {
-       id:6,
-       value:'14:00'
-   }, {
-       id:7,
-       value:'15:00'
-   }, {
-       id:8,
-       value:'16:00'
-    }
-]
+const renderTimeOptions = (context, timeOptions) => {
 
-const renderTimeOptions = context => {
-
-    const targetElement = context.querySelector('.options')
+    const targetElement = context.querySelector(".options")
 
     targetElement.innerHTML = ""
 
-    data.forEach(item => {
+    timeOptions.forEach(item => {
 
         appendTemplate(
             targetElement,
             "label",
             `
-                    <input type="radio" name="option" value="${item.value}">
-                    <span>${item.value}</span>
+                <input type="radio" name="option" value="${item.value}" />
+                <span>${item.value}</span>
             `
         )
     })
 }
 
 const validateSubmitForm = context => {
+
     const button = context.querySelector("[type=submit]")
 
-    const checkValue = () => {
-        if (context.querySelector("[name=option]:checked")) {
-            button.disabled = false
-        } else {
-            button.disabled = true
-        }
-    };
+    context.querySelectorAll("[name=option]").forEach(input => {
 
-    window.addEventListener("load", e => checkValue())
-
-    context.querySelectorAll("[name=option]").forEach((input) => {
         input.addEventListener("change", e => {
-            
-            checkValue()
-        });
-    });
+            button.disabled = !context.querySelector("[name=option]:checked")        
+        })
+
+    })
 
     context.querySelector("form").addEventListener("submit", e => {
+
         if (!context.querySelector("[name=option]:checked")) {
             button.disabled = true
             e.preventDefault()
         }
-    });
-};
+
+    })
+
+}
 
 document.querySelectorAll('#time-options').forEach(page => {
-    renderTimeOptions(page)
+
+    const db = firebase.firestore()
+
+    db.collection('time-options').onSnapshot(snapshot => {
+
+        const renderData = []
+
+        snapshot.forEach(item => {
+            renderData.push(item.data())
+        })
+
+        renderTimeOptions(page, renderData)
+    })
 
     validateSubmitForm(page)
 
     const params = getQueryString()
     const title = page.querySelector('h3')
-    const form = page.querySelector("form");
+    const form = page.querySelector("form")
     const scheduleAt = parse(params.schedule_at, "yyyy-MM-dd", new Date())
 
     setFormValues(form, params)
